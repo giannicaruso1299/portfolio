c1398969149274d71fa8ac488a764d27e7301035
giannicaruso99 <giannicaruso1897@gmail.com>
2022-03-27 19:38:42 +0200

    Quasi finito il curriculum

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: Title) { }
 
   ngOnInit(): void {
+  }
+
+  onSectionClick(section: string) {
+    this.titleService.setTitle(`${section} | Giovan Battista Caruso`);
   }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  scrolled = false;
+  scrolled2 = false;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll($event: Event) {
+    const numb = window.scrollY;
+    this.scrolled = numb >= window.innerHeight / 2;
+    this.scrolled2 = numb >= window.innerHeight;
+  }
+
+  constructor(private titleService: Title) { }
 
   ngOnInit(): void {
+    this.titleService.setTitle('Home | Giovan Battista Caruso');
   }
 
 }
